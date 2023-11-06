420db27f0f020054673f8fd61bd74f782bf2fff6
Andrew Bourguignon <theakbmarketing@gmail.com>
2023-11-05 19:44:41 -0500

    Initial Commit

---
+++ NowPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NowPlayer
+//
+//  Created by Andrew Bourguignon on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NowPlayer/NowPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayerApp.swift
+//  NowPlayer
+//
+//  Created by Andrew Bourguignon on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
