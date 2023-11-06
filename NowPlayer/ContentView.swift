327e1e0aa429622df7240107d4d0c773bdd4eb9a
Andrew Bourguignon <theakbmarketing@gmail.com>
2023-11-05 19:55:54 -0500

    Initial commit

--- NowPlayer/ContentView.swift
+++ NowPlayer/ContentView.swift
@@ -1,24 +1,60 @@
-//
-//  ContentView.swift
-//  NowPlayer
-//
-//  Created by Andrew Bourguignon on 11/5/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nowPlayingInfo = "No audio playing"
+    @State private var windowOnTop = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Now Playing")
+                .font(.title)
+
+            Image(systemName: "music.note") // Placeholder icon
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50.0)
+                .padding()
+
+            Text(nowPlayingInfo)
+                .font(.subheadline)
+                .padding()
+
+            Button("Keep on Top") {
+                if let window = NSApp.windows.first {
+                    if windowOnTop {
+                        window.level = .normal
+                    } else {
+                        window.level = .floating
+                    }
+                    windowOnTop.toggle()
+                }
+            }
+            .padding()
+
+            Button("Play/Pause") {
+                // Add code here to toggle between play and pause
+                // For example, you can use AppleScript to control playback in some media players
+            }
+            .padding()
+
+            HStack {
+                Button("Rewind") {
+                    // Add code here to rewind the current track
+                }
+                .padding()
+
+                Button("Next Track") {
+                    // Add code here to play the next track
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
